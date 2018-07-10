999b55323803ced1daa2e55616548d4f845625c8
Rob Gravelle <rob@robgravelle.com>
2018-07-10 08:16:50 -0400

    Added src folder.

---
+++ src/components/Msg.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="msg">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyMessage',
+  data () {
+    return {
+      msg: 'Welcome Rob!'
+    }
+  }
+}
+</script>
